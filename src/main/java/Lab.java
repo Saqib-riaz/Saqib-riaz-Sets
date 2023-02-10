33ab80babb9112db711859c31994e9ec11cb2bd7
Saqib-riaz <likenw@gmail.com>
2023-02-10 23:39:52 +0000

    done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,4 @@
-
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -20,7 +20,7 @@ public class Lab {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        return new HashSet<>();
     }
 
     /**
@@ -29,26 +29,26 @@ public class Lab {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        return set.size();
     }
 
     /**
      * Add a new, unique item to a set. Sets can only store unique items (no duplicates).
      * @param set The set that we will be manipulating.
      * @param item The item which we will be adding to set.
-     * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
+     * @return nothing, pass by reference will cause changes to the set object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
-
+        set.add(item);
     }
 
     /**
      * Remove an item from a set.
      * @param set The set that we will be manipulating.
      * @param item The item which we will be removing from set.
-     * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
+     * @return nothing, pass by reference will cause changes to the set object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        set.remove(item);
     }
 }
